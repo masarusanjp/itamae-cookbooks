fdac0563f064ca114c7af8e14745353259fa127a
masaichi <masarusanplusplus@gmail.com>
2017-02-19 20:37:43 +0900

    fixed variable

--- docker/default.rb
+++ docker/default.rb
@@ -3,7 +3,7 @@ apt_key_keys = '58118E89F3A912897C070ADBF76221572C52609D'
 apt_key_server = 'hkp://p80.pool.sks-keyservers.net:80'
 execute "apt-key #{apt_key_name}" do
   command "apt-key adv --keyserver #{apt_key_server} --recv-keys #{apt_key_keys}"
-  not_if "apt-key list | grep -q '\#{name} '"
+  not_if "apt-key list | grep -q '\#{apt_key_name} '"
 end
 
 file '/etc/apt/sources.list.d/docker-engine.list' do
